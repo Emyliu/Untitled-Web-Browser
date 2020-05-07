07cedcc613d7de06eef1bb29f595d82a0be2dcd7
Emyliu <32317817+Emyliu@users.noreply.github.com>
2020-05-07 10:27:50 -0400

    Implementation for back/forward, doesn't work yet.

--- app/nav.js
+++ app/nav.js
@@ -1,8 +1,40 @@
 // Back, Forward, Refresh.
+let browser = {
+    currentHistory = [],
+    currentPosition = -1
+}
 
-// Back and Forward are going to need IPC w/ the database so just begin with the refresh
+document.getElementById("mainFrame").addEventListener("beforeunload", function(event) {
+// Really, not the best way to do this - but all I have for now.
+push(document.getElementById("mainFrame").src)
+});
+
+function push(url) {
+    browser.currentHistory.push(url)
+}
+
 
 function refreshPage() {
     let ref = document.getElementById("mainFrame").src
     document.getElementById("mainFrame").src = ref
+}
+
+// IPC returns the correct page, don't change it if nothing to change.
+// Button SHOULD be disabled if we're at the limit.
+function back() {
+    if (browser.currentPosition <= 0) {
+        // do nothing.
+    } else {
+        currentPosition = currentPosition - 1
+        document.getElementById("mainFrame").src = currentHistory[browser.currentPosition]
+    }
+}
+
+function forward() {
+    if (browser.currentPosition + 1 >= browser.currentHistory.length) {
+        // do nothing.
+    } else {
+        currentPosition = currentPosition + 1
+        document.getElementById("mainFrame").src = currentHistory[browser.currentPosition]
+    }
 }
